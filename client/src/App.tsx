b22d6fef945e48f9278504eeaf7f14013b574e21
Nitin Pathak <nitinpathak211@gmail.com>
2026-08-09 23:03:59 +0530

    feat: integrate client with api

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,8 +1,22 @@
-import './App.css'
+import { useState, useEffect } from "react";
+import "./App.css"
 
 function App() {
+  const [message, setMessage] = useState("");
+
+  useEffect(() => {
+    fetch("http://localhost:5000/api/health")
+      .then((response) => response.json())
+      .then((data) => {
+        setMessage(data.message);
+      })
+  },[]);
+
   return (
-    <div>Task flow application coming soon</div>
+    <div className="app-container">
+      <h2>Task flow application coming soon</h2>
+      <p>{message}</p>
+    </div>
   )
 }
 
