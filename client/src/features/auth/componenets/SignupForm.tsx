836a0f8b0b0631847f2ef72c670b826ddfb954a9
Nitin Pathak <nitinpathak211@gmail.com>
2026-08-14 16:39:18 +0530

    feat(auth): add form loading and error states (#9)

--- client/src/features/auth/componenets/SignupForm.tsx
+++ client/src/features/auth/componenets/SignupForm.tsx
@@ -1,13 +1,25 @@
-import { useState } from "react"
+import { useState, type SubmitEvent } from "react"
 
 function SignupForm() {
+    const [ error, setError ] = useState<string | null>(null);
+    const [ isloading, setIsLoading ] = useState(false);
     const [name, setName] = useState<string>("");
     const [emailId, setEmailId] = useState<string>("");
     const [password, setPassword] = useState<string>("");
     const [confirmPassword, setConfirmPassword] = useState<string>("");
 
-    const handleSignupSubmit = () => {
-        console.log("handleSignupSubmit");
+    const handleSignupSubmit = async (event:SubmitEvent<HTMLFormElement>) => {
+        event.preventDefault();
+        setError(null);
+        setIsLoading(true);
+
+        try {
+            await new Promise((resolve) => setTimeout(resolve,1500));
+        } catch {
+            setError("Something went wrong, Please try again");
+        } finally {
+            setIsLoading(false);
+        }
     };
     return (
         <form className="auth-form" onSubmit={handleSignupSubmit}>
@@ -59,8 +71,19 @@ function SignupForm() {
                     placeholder="Enter your password again"
                 />
             </div>
+            {
+                error && (
+                    <p className="auth-error" role="alert">{error}</p>
+                )
+            }
             <div>
-                <button className="btn btn-primary" type="submit">Singn up</button>
+                <button 
+                    className="btn btn-primary" 
+                    type="submit"
+                    disabled={isloading}
+                >
+                    { isloading ? "Creating account..." : "Create account"}
+                </button>
             </div>
         </form>
     )
