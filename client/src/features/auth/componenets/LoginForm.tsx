0bee157b83ebc68d0f4c024ab369b51fb5335b6d
Nitin Pathak <nitinpathak211@gmail.com>
2026-08-14 15:28:39 +0530

    feat(auth): add form loading and error states

--- client/src/features/auth/componenets/LoginForm.tsx
+++ client/src/features/auth/componenets/LoginForm.tsx
@@ -1,43 +1,67 @@
 import { useState, type SubmitEvent } from "react";
 
 function LoginForm() {
-    const [email, setEmail] = useState<string>("");
-    const [password, setPassword] = useState<string>("");
+  const [email, setEmail] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
+  const [ isLoading, setIsLoading ] = useState(false);
+  const [ error, setError ] = useState<string|null>(null);
 
-    const handleSubmit = (event:SubmitEvent<HTMLFormElement>) => {
-        event.preventDefault();
-        console.log("handleLoginSubmit");
+  const handleSubmit = async (event:SubmitEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    setError
+    setIsLoading(true);
+
+    try {
+      // temprory simulation untill we connect with api
+      await new Promise((resolve) => setTimeout(resolve,1500));
+    } catch {
+      setError("Something went wrong. Please try again.");
+    } finally {
+      setIsLoading(false);
     }
-    return (
-        <form className="auth-form" onSubmit={handleSubmit}>
-            <div className="form-field">
-                <label htmlFor="email">Email</label>
-                <input 
-                    id="email"
-                    type="email"
-                    value={email}
-                    onChange={(event) => setEmail(event.target.value)}
-                    required
-                    placeholder="Enter your email address"
-                />
-            </div>
-            <div className="form-field">
-                <div className="password-header">
-                     <label htmlFor="password">Password</label>
-                     <a href="/forgot-password">Forgot Password</a>
-                </div>
-                <input
-                    id="password"
-                    type="password"
-                    value={password}
-                    onChange={(event) => setPassword(event.target.value)}
-                    required
-                    placeholder="Enter your password"
-                />
-            </div>
-            <button className="btn btn-primary" type="submit">Log in</button>
-        </form>
-    )
+  }
+  return (
+    <form className="auth-form" onSubmit={handleSubmit}>
+      <div className="form-field">
+          <label htmlFor="email">Email</label>
+          <input 
+              id="email"
+              type="email"
+              value={email}
+              onChange={(event) => setEmail(event.target.value)}
+              required
+              placeholder="Enter your email address"
+          />
+      </div>
+      <div className="form-field">
+          <div className="password-header">
+                <label htmlFor="password">Password</label>
+                <a href="/forgot-password">Forgot Password</a>
+          </div>
+          <input
+              id="password"
+              type="password"
+              value={password}
+              onChange={(event) => setPassword(event.target.value)}
+              required
+              placeholder="Enter your password"
+          />
+      </div>
+      {
+        error && (
+          <p className="auth-error" role="alert">
+            {error}
+          </p>
+        )
+      }
+      <button 
+        className="btn btn-primary" 
+        type="submit"
+      >
+        { isLoading ? "Loggin in..." : "Log in" }
+      </button>
+    </form>
+  )
 }
 
 export default LoginForm;
